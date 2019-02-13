6d75bae477a082a7d1dd7eca411e5bec154a2063
CodeX97 <thamindu.randil@gmail.com>
2019-02-14 00:23:26 +0530

    register_database ok

--- login.php
+++ login.php
@@ -1,28 +1,29 @@
 <?php
-if($_SERVER["REQUEST_METHOD"]=="post"){
-    $username = mysql_real_escape_string($_POST['uname']);
-    $password = mysql_real_escape_string($_POST['psw']);
+if($_SERVER["REQUEST_METHOD"]=="POST"){
+    $db = mysqli_connect("localhost:3306","root","", "1st_db") or die(mysql_error()); //connect to server & DB
+
+    $username = mysqli_real_escape_string($db, $_POST['uname']);
+    $password = mysqli_real_escape_string($db, $_POST['psw']);
     $bool = true;
 
-    mysql_connect("localhost","root","") or die(mysql_error()); //connect to server
-    mysql_select_db("1st_db") or die("Can't connect to databse"); //connect to databse
-    $query = mysql_query("Select * from users"); //query the user table
-    while($row = mysql_fetch_array($query)) //display all rows from query
+    //mysql_select_db() or die("Can't connect to databse"); //connect to databse
+    $query = mysqli_query($db, "Select * from users"); //query the user table
+    while($row = mysqli_fetch_array($query, MYSQLI_ASSOC)) //display all rows from query
     {
         $table_user = $row['username']; //first username row is passed to the variable
         if($username==$table_user)
         {
             $bool = false;
             Print '<script>alert("Username has been taken !");</script>'; //prompt the user
-            Print '<script>window.location.assign("register.html");</script>'; //redirects to register page
+            Print '<script>window.location.assign("login.php");</script>'; //redirects to register page
 
         }
     }
     if($bool)
     {
-        mysql_query("INSERT INTO users (username, password) VALUES ('$username'.'$password')"); //insert values to table user
+        mysqli_query($db, "INSERT INTO users (username, password) VALUES ('$username', '$password')"); //insert values to table user
         Print '<script>alert("Successfully Registered !");</script>';
-        Print '<script>window.location.assign("register.html");</script>';
+        Print '<script>window.location.assign("login.html");</script>';
 
     }
 }
