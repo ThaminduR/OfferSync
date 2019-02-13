a714dab8bf5020815b548f2c9a748f50a9ebe007
CodeX97 <thamindu.randil@gmail.com>
2019-02-13 23:13:50 +0530

    Login php testing phase 1

--- login.php
+++ login.php
@@ -1,7 +1,29 @@
-<!DOCTYPE HTML>
-<head>
-<body>
-    Welcome <?php echo $_POST["uname"]; ?><br>
-    Your password is: <?php echo $_POST["psw"]; ?>
-</body>
-</head>
+<?php
+if($_SERVER["REQUEST_METHOD"]=="post"){
+    $username = mysql_real_escape_string($_POST['uname']);
+    $password = mysql_real_escape_string($_POST['psw']);
+    $bool = true;
+
+    mysql_connect("localhost","root","") or die(mysql_error()); //connect to server
+    mysql_select_db("1st_db") or die("Can't connect to databse"); //connect to databse
+    $query = mysql_query("Select * from user"); //query the user table
+    while($row = mysql_fetch_array($query)) //display all rows from query
+    {
+        $table_user = $row['username']; //first username row is passed to the variable
+        if($username==$table_user)
+        {
+            $bool = false;
+            Print '<script>alert("Username has been taken !");</script>'; //prompt the user
+            Print '<script>window.location.assign("register.html");</script>'; //redirects to register page
+
+        }
+    }
+    if($bool)
+    {
+        mysql_query("INSERT INTO users (username, password) VALUES ('$username'.'$password')"); //insert values to table user
+        Print '<script>alert("Successfully Registered !");</script>';
+        Print '<script>window.location.assign("register.html");</script>';
+
+    }
+}
+?>
