1c79aa1bd861beb3320c44c684884b80fc6b5206
CodeX97 <thamindu.randil@gmail.com>
2019-02-14 14:16:17 +0530

    register php edited

--- register.php
+++ register.php
@@ -1,7 +1,23 @@
 <?php
 if($_SERVER["REQUEST_METHOD"]=="POST"){
     $db = mysqli_connect("localhost:3306","root","", "1st_db") or die(mysql_error()); //connect to server & DB
+    class user{
+        private var $firstname;
+        private var $lastname;
+        private var $city;
+        private var $username;
+        private var $password;
 
+        function __construct($first_name,$last_name,$_city,$uname,$psw){
+            $this->firstname = $first_name;
+            $this->lastname = $last_name;
+            $this->city = $_city;
+            $this->username = $uname;
+            $this->password = $psw;
+        }
+
+
+    }
     $username = mysqli_real_escape_string($db, $_POST['uname']);
     $password = mysqli_real_escape_string($db, $_POST['psw']);
     $bool = true;
