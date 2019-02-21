5be6776866cd1a1fda0de578942e61b30fa1198a
CodeX97 <thamindu.randil@gmail.com>
2019-02-21 14:41:11 +0530

    file system changed

--- public/index.php
+++ public/index.php
@@ -4,17 +4,17 @@ require_once('Routes.php');
 
 function __autoload($class_name)
 {
-  if (file_exists('./classes/'.$class_name.'.php'))
+  if (file_exists('./src/app/'.$class_name.'.php'))
   {
-    require_once './classes/'.$class_name.'.php';
+    require_once './src/app/'.$class_name.'.php';
   }
   else if(file_exists('./src/controllers/'.$class_name.'.php'))
   {
     require_once './src/controllers/'.$class_name.'.php';
   }
-  else if(file_exists('./models/Login/'.$class_name.'php'))
+  else if(file_exists('./src/models/Login/'.$class_name.'php'))
   {
-    require_once './models/Login/'.$class_name.'php';
+    require_once './src/models/Login/'.$class_name.'php';
   }
 }
 
