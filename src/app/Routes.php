029c4fa84a0518dcfec3a1e95f941ad82368bc9b
CodeX97 <thamindu.randil@gmail.com>
2019-03-06 17:56:07 +0530

    Test

--- src/app/Routes.php
+++ src/app/Routes.php
@@ -34,3 +34,14 @@ Route::set('RegisterController',function()
 {
     RegisterController::SignUp();
 });
+
+//---------------------Testing Routing for Klein Library-------------//
+require $_SERVER['DOCUMENT_ROOT'].'/..' . '/src/includes/Klein/klein.php'
+$routes = [
+    ['GET','$_SERVER['DOCUMENT_ROOT'].'/..' . '/src/controllers/RegisterController.php'','SignUp()']
+    ['GET','$_SERVER['DOCUMENT_ROOT'].'/..' . '/src/controllers/LoginController.php'','LogIn()']
+    
+]
+
+$router = new klein();
+$router->respond()
