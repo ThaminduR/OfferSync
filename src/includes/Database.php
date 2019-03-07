365e656f306cb54aad8cdd6f20c1dc5e9971a851
CodeX97 <thamindu.randil@gmail.com>
2019-03-07 12:58:16 +0530

    Database Library

--- src/includes/Database.php
+++ src/includes/Database.php
@@ -11,12 +11,13 @@ class Database{
     private $dbname;
     private $connection;
 
-    public Database(){
-        this->server = server;
-        this->user = user;
-        this->password = password;
-        this->dbname = db;
-        this->Connect(); 
+    public function __construct(){
+        $this->Database = $this;
+        $this->server = server;
+        $this->user = user;
+        $this->password = password;
+        $this->dbname = db;
+        $this->Connect(); 
     }
     private function Connect(){
         $this->connection = new mysqli($this->server, $this->user, $this->password, $this->dbname);
@@ -27,11 +28,32 @@ class Database{
         
     }
     //Insert function - required parameter is an array of the data to to be inserted.
-    public function Insert($){
-        
+    public function InsertUserDetail($username,$firstname,$lastname,$gender,$email,$city){
+        $sql="INSERT INTO users SET username='$username', firstname='$firstname', lastname='$lastname', city='$city', gender='$gender', email='$email'";
+        $result = mysqli_query($this->connection,$sql) or die("Data cannot inserted");
+        return $result;
+    }
+
+    public function InsertLoginData($username,$password,$salt){
+        $sql="INSERT INTO userlogin SET username='$username', password='$password', salt='$salt'";
+        $result = mysqli_query($this->connection,$sql) or die("Data cannot inserted");
+        return $result;
     }
     
-    public function Find(){
+    public function FindUserDetail($username){
+        $sql="SELECT * FROM userlogin WHERE username='$username'";
+        $result = mysqli_query($this->connection,$sql);
+        $user_data = mysqli_fetch_array($result);
+        return $user_data;    
+    }
 
+    public function FindUser($username,$email){
+        $sql="SELECT * FROM userlogin WHERE username='$username' or email='$email'";
+        $result = mysqli_query($this->connection,$sql);
+        if (empty($result)){
+            return false;
+        } else {
+            return true;
+        }
     }
 }
--- src/models/classes/User.php
+++ src/models/classes/User.php
@@ -1,7 +1,7 @@
 <?php
 require_once $_SERVER['DOCUMENT_ROOT']. '/..'. '/src/includes/database_config.php';
 require_once $_SERVER['DOCUMENT_ROOT']. '/..'. '/src/includes/salt.php';
-
+require_once $_SERVER['DOCUMENT_ROOT']. '/..'. '/src/includes/Database.php';
 
 
 class User
@@ -12,56 +12,33 @@ class User
 
     public function __construct()
     {   
-
-        $this->database = new mysqli(server, user, password, db);
-        if(mysqli_connect_errno()) {
-            echo "Error: Could not connect to database.";
-                exit;
-        }
+        $this->database = new Database();
+        
     }
 
-    /*** for login ***/
+    //For Login
     public function check_login($username, $password){
-
-        
-        $sql1="SELECT * FROM userlogin WHERE username='$username'";
-
-        //checking if the username is available in the table
-        $result = mysqli_query($this->database,$sql1);
-        $user_data = mysqli_fetch_array($result);
-        //getting user data from array
-        //$useremail = $user_data['email'];
+        $user_data = $this->database->FindUserDetail($username);
         $db_hpassword = $user_data['password'];
         $salt = $user_data['salt'];
         $hpassword = base64_encode(hash('sha256',"$username.$password.$salt.$this->pepper",TRUE));        
-        //compare two hashed passwords
         if (strcmp($hpassword,$db_hpassword) == 0) {
-            // this login var will use for the session thing
             //$this->$_SESSION['login'] = TRUE;
-            //$this->$_SESSION['id'] = $user_data['id'];
             return true;
         }
         else{
             return false;
         }
     }
-
+    //For Registration
     public function Reg_User($username,$firstname,$lastname,$email,$gender,$city,$password){
         $salt = getSalt(25);
         $hpassword = base64_encode(hash('sha256',"$username.$password.$salt.$this->pepper",TRUE));
-        
-        $sql2="SELECT * FROM users WHERE uname='$username' OR uemail='$email'";
-    
-        //checking if the username or email is available in db
-        $check =  mysqli_query($this->database,$sql2) ;
-        
-        //if the username is not in db then insert to the table
-        if (empty($check)){
-            $sql1="INSERT INTO users SET username='$username', firstname='$firstname', lastname='$lastname', city='$city', gender='$gender', email='$email'";
-            $sql2="INSERT INTO userlogin SET username='$username', password='$hpassword', salt='$salt'";
-            $result1 = mysqli_query($this->database,$sql1) or die("Data cannot inserted");
-            $result2 = mysqli_query($this->database,$sql2) or die("Data cannot inserted");
-                      //mysqli_connect_errno() - put this in die to find error code
+        $check =  $this->database->FindUser($username,$email);
+        echo $check;
+        if (!($check)){
+            $result1 = $this->database->InsertUserDetail($username,$firstname,$lastname,$gender,$email,$city);
+            $result2 = $this->database->InsertLoginData($username,$hpassword,$salt);
             return $result1;
         }
         else { return false;}
