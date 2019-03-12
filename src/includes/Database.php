6f8e0d21d7f741b55f879d39dbb7cb93d44594b1
CodeX97 <thamindu.randil@gmail.com>
2019-03-12 12:34:04 +0530

    minor changes - database file

--- src/includes/Database.php
+++ src/includes/Database.php
@@ -4,15 +4,14 @@
 require_once $_SERVER['DOCUMENT_ROOT']. '/..'. '/src/includes/database_config.php';
 
 class Database{
-    private $Database;
     private $server;
     private $user;
     private $password;
     private $dbname;
     private $connection;
+    private $Database;
 
     public function __construct(){
-        $this->Database = $this;
         $this->server = server;
         $this->user = user;
         $this->password = password;
@@ -29,18 +28,26 @@ class Database{
     }
     //Insert function - required parameter is an array of the data to to be inserted.
     public function InsertUserDetail($username,$firstname,$lastname,$gender,$email,$city){
+        $username = mysqli_real_escape_string($this->connection,$username);
+        $firstname = mysqli_real_escape_string($this->connection,$firstname);
+        $lastname = mysqli_real_escape_string($this->connection,$lastname);
+        $gender = mysqli_real_escape_string($this->connection,$gender);
+        $email = mysqli_real_escape_string($this->connection,$email);
+        $city = mysqli_real_escape_string($this->connection,$city);
         $sql="INSERT INTO users SET username='$username', firstname='$firstname', lastname='$lastname', city='$city', gender='$gender', email='$email'";
         $result = mysqli_query($this->connection,$sql) or die("Data cannot inserted");
         return $result;
     }
 
     public function InsertLoginData($username,$password,$salt){
+        $username = mysqli_real_escape_string($this->connection,$username);
         $sql="INSERT INTO userlogin SET username='$username', password='$password', salt='$salt'";
         $result = mysqli_query($this->connection,$sql) or die("Data cannot inserted");
         return $result;
     }
     
     public function FindUserDetail($username){
+        $username = mysqli_real_escape_string($this->connection,$username);
         $sql="SELECT * FROM userlogin WHERE username='$username'";
         $result = mysqli_query($this->connection,$sql);
         $user_data = mysqli_fetch_array($result);
@@ -48,6 +55,8 @@ class Database{
     }
 
     public function FindUser($username,$email){
+        $username = mysqli_real_escape_string($this->connection,$username);
+        $email = mysqli_real_escape_string($this->connection,$email);
         $sql="SELECT * FROM userlogin WHERE username='$username' or email='$email'";
         $result = mysqli_query($this->connection,$sql);
         if (empty($result)){
