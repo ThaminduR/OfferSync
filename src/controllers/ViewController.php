b64f741827b849e17ec4c146f77c8eec70decd04
CodeX97 <thamindu.randil@gmail.com>
2019-05-08 21:37:13 +0530

    a

--- src/controllers/ViewController.php
+++ src/controllers/ViewController.php
@@ -6,14 +6,6 @@ require_once $_SERVER['DOCUMENT_ROOT'] . '/..' . '/src/includes/LoginStatus.php'
 class ViewController
 {
     public static function CreateView($viewName)
-<<<<<<< HEAD
-    {   
-        if ($viewName=='search_result'){
-        require_once($_SERVER['DOCUMENT_ROOT']. '/..'. '/src/view/'.$viewName.'.php'); 
-        }
-
-        require_once($_SERVER['DOCUMENT_ROOT']. '/..'. '/src/view/'.$viewName.'.html');
-=======
     {
         if ($viewName == 'index') {
             $logged = CheckLoginStatus();
@@ -25,7 +17,6 @@ class ViewController
         } else {
                 require_once($_SERVER['DOCUMENT_ROOT'] . '/..' . '/src/view/' . $viewName . '.html');
             }
->>>>>>> 052bd3ec5892b96c2ac704fab1c51ebec625795f
     }
 
     public static function CreateViewR($viewName)
