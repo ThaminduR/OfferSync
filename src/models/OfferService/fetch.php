76007538429f6027a3ac6ce0c1dee333a85d38fd
nigma97 <isuranga.17@cse.mrt.ac.lk>
2019-05-26 20:31:23 +0530

    lkk

--- src/models/OfferService/fetch.php
+++ src/models/OfferService/fetch.php
@@ -24,7 +24,6 @@ function SearchOffers()
 				<div class="col-4 offerdetails">
 					<ul >
 					<form id="form1" action="/AccountView" method="POST">
-<<<<<<< HEAD
 					
 						<a href="javascript:submitform()"><li><i class="fas fa-user-circle"></i><span class="ml-2">'.$row["Username"].'</span></li></a>
 						<input type="hidden"  name="Username" value="'.$row["Username"].'">
@@ -33,13 +32,6 @@ function SearchOffers()
 						<li><i class="fas fa-map-marker-alt"></i> <span class="ml-2">'.$row["Restaurant"].'</span></li>
 						<li><i class="fas fa-map-marker-alt"></i> <span class="ml-2">'.$row["City"].'</span></li>
 						<li><i class="fas fa-building"></i><span class="ml-2">'.$row["Date"].'</span></li>
-=======
-						<a href="javascript: submitform()"><li><i class="fas fa-user-circle"></i><span class="ml-2">' . $row["Username"] . '</span></li></a>
-					</form>
-						
-						<li><i class="fas fa-map-marker-alt"></i> <span class="ml-2">' . $row["City"] . '</span></li>
-						<li><i class="fas fa-building"></i><span class="ml-2">' . $row["Date"] . '</span></li>
->>>>>>> b7eecd1ac4ef2bc44f89939aaa35bff4d8d4cfc4
 						
 					</ul>
 				</div>
@@ -69,7 +61,6 @@ function SearchOffers()
 	}
 }
 
-<<<<<<< HEAD
 
 
 function DisplayUser($username)
@@ -130,9 +121,3 @@ function DisplayUser($username)
 	
 }
 
-=======
-function SendRequest()
-{ 
-
-}
->>>>>>> b7eecd1ac4ef2bc44f89939aaa35bff4d8d4cfc4
