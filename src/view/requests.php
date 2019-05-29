721cfc8dc8faa3b5393e1f3871812b027f2b095e
CodeX97 <thamindu.randil@gmail.com>
2019-05-29 13:53:35 +0530

    Minor

--- src/view/requests.php
+++ src/view/requests.php
@@ -39,7 +39,7 @@
     <div class="bg">
         <div class="container">
             <div class="col-12">
-                <div class="row-3"><input onblur="Accept()">CLick to view</input></div>
+                <div class="row-3"><a class="btn" onclick="load_requests()">CLick to view</a></div>
                 <div class="row-9">
                     <div id="result"></div>
                     <div style="clear:both"></div>
@@ -58,13 +58,13 @@
 
     <!-- SCRIPTS -->
     <!-- JQuery -->
-    <script type="text/javascript" src="js/jquery-3.4.0.min.js"></script>
+    <script type="text/javascript" src="js/jquery-3.3.1.min.js"></script>
     <!-- Bootstrap tooltips -->
     <script type="text/javascript" src="js/popper.min.js"></script>
     <!-- Bootstrap core JavaScript -->
     <script type="text/javascript" src="js/bootstrap.min.js"></script>
     <!-- MDB core JavaScript -->
-    <script type="text/javascript" src="js/mdb.min.js"></script>
+    <script type="text/javascript" src="js/mdb.js"></script>
     <!-- Initializations -->
     <script>
         function load_requests() {
