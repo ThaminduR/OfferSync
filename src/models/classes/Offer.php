53d8d92e4ac3f92ae52d69beab57b4f669785c6c
nigma97 <isuranga.17@cse.mrt.ac.lk>
2019-05-30 08:43:59 +0530

    offer object1

--- src/models/classes/Offer.php
+++ src/models/classes/Offer.php
@@ -1,15 +1,36 @@
 <?Php
 
-class Offer {
-    var $username;
-    var $City;
-    var $Restaurant;
-    
-    public function Post(){
+//..........Offer Object Factory..........
 
+class Offer
+{
+    private $Username;
+    private $City;
+    private $Offer;
+    private $Restaurant;
+    private $Price;
+    private $Gender;
+    private $RestaurantBranch;
+    private $Date;
+
+
+    public function __construct($Username, $City, $Offer, $Restaurant, $Price, $Gender, $RestaurantBranch, $Date)
+    {
+        $this->Username = $Username;
+        $this->City = $City;
+        $this->Offer = $Offer;
+        $this->Restaurant = $Restaurant;
+        $this->Price = $Price;
+        $this->Gender = $Gender;
+        $this->RestaurantBranch = $RestaurantBranch;
+        $this->Date = $Date;
+    }
+    public function Post($database)
+    {
+        $offers = $database->PostOffer($this->Username, $this->Restaurant, $this->Offer, $this->Price, $this->RestaurantBranch, $this->Date, $this->City, $this->Gender);
+        header("location:/");
     }
 
-    public function Search(){
-        
-    }
+    public function Search()
+    { }
 }
