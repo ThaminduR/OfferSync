9bcaf1a8a8d13fbcb44694aa6741d892623b41cd
MithunW <mithun.17@cse.mrt.ac.lk>
2019-05-30 08:56:02 +0530

    controller get data

--- src/view/editProfile.php
+++ src/view/editProfile.php
@@ -85,7 +85,7 @@
                         <li class="selected">
                             <div class="heading">
                                 <h1>Update Your Email</h1>
-                                <form class="text-center p-5" action="/RegisterController" method="POST">
+                                <form class="text-center p-5" action="/AccountController" method="POST">
                                     <!-- Card -->
                                     <div class="card">
                                         <!-- Card Content -->
